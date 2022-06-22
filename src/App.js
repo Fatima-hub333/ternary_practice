7078ae2c1a89cf8596b9144c48e6df5709c39c0a
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-22 15:26:37 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import './index.css';
 export default function App() {
   const isGoingOut = true;
 
-  const answer = isGoingOut ? 'Yes' : 'No';
-
   return (
     <div className="state">
       <h1 className="state--title">Do I feel like going out tonight?</h1>
       <div className="state--value">
-        <h1>{answer}</h1>
+        <h1>{isGoingOut ? 'Yes' : 'No'}</h1>
       </div>
     </div>
   );
