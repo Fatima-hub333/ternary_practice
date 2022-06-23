03366e6d161cd30776190686b5d0418dfae092b6
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-23 12:32:59 +0500

    Practice flipping state back & forth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+/* eslint-disable no-confusing-arrow */
+/* eslint-disable no-unneeded-ternary */
+/* eslint-disable arrow-parens */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import './index.css';
 
+/**
+     * Challenge:
+     * - Initialize state for `isGoingOut` as a boolean
+     * - Make it so clicking the div.state--value flips that
+     *   boolean value (true -> false, false -> true)
+     * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
+     */
+
 export default function App() {
-  const isGoingOut = true;
+  const [isGoingOut, setIsGointOut] = React.useState(true);
+
+  function changeMind() {
+    setIsGointOut(prevState => !prevState);
+  }
 
   return (
     <div className="state">
       <h1 className="state--title">Do I feel like going out tonight?</h1>
-      <div className="state--value">
+      <div onClick={changeMind} className="state--value">
         <h1>{isGoingOut ? 'Yes' : 'No'}</h1>
       </div>
     </div>
